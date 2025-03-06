26257c91b69fcf672db27d6615c5712548566444
luanfs <luanfonseca93@gmail.com>
2025-03-06 09:03:48 -0500

    compilation scripts update

--- scripts/compilation/compile_hydro.sh
+++ scripts/compilation/compile_hydro.sh
@@ -2,7 +2,7 @@
 # Script to compile the non hydrostatic solver
 
 if [ "$#" -ne 3 ]; then
-  echo "Usage: ./compile_nh.sh COMP clean mode"
+  echo "Usage: ./compile_hydro.sh COMP clean mode"
   echo "choices for COMP :  debug, repro, prod"
   echo "choices for clean:  noclean, clean, cleanall"
   echo "choices for mode :  32bit, 64bit"
@@ -13,10 +13,9 @@ clean="$2"
 mode="$3"
 compiler="intel"
 
-model=nh
+model=hydro
 cd ../../SHiELD_build/Build
 ./COMPILE solo $model $COMP $compiler $mode $clean
 
 # Copy the log file to test directory
 cp -r build_solo_$model.$COMP.$mode.$compiler.out ../../scripts/compilation/build_solo_$model.$COMP.$mode.$compiler.out
-
